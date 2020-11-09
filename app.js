0ae2a000b888b385549b231a34369b129d306662
julian-leahy <julian.leahy.dev@gmail.com>
2020-11-10 12:33:31 +1300

    Delete user from collection

--- app.js
+++ app.js
@@ -36,15 +36,25 @@ const generateUserList = (doc => {
     const li = document.createElement('li');
     const name = document.createElement('span');
     const email = document.createElement('span');
+    const deleteX = document.createElement('div');
     // add firestore generated id so we can reference if required
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     email.textContent = doc.data().email;
+    deleteX.textContent = 'X';
+
 
     li.appendChild(name);
     li.appendChild(email);
+    li.appendChild(deleteX);
 
     userList.appendChild(li);
+
+    // delete user
+    deleteX.addEventListener('click', (e) => {
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collection('users').doc(id).delete();
+    })
 })
 
 
