076e07b8da2b68948819bc831c83e7b8f18964e7
julian-leahy <julian.leahy.dev@gmail.com>
2020-11-10 11:57:24 +1300

    Add new document to a collection

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const form = document.querySelector('#add-user');
+
+
 /**
  * Return all documents from a collection
  * @param {string} document 
@@ -8,8 +11,24 @@ const getAllDocs = (document) => {
     })
 }
 
+/**
+ * Add a new document to a collection
+ * @param {string} document 
+ * @param {object} obj 
+ */
+const addDocument = (document, obj) => {
+    db.collection(document).add(obj);
+}
 
 
-
+// TEST
 
 let userDocs = getAllDocs('users');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addDocument('users', {
+        name: form.name.value,
+        email: form.email.value
+    })
+})
